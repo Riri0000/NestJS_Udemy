dd6e1604ecf72c18df958f83bb851649e7342dd5
Riri0000 <ririko010102@gmail.com>
2022-01-08 16:50:03 +0900

    Add: routes

--- scratch/src/app.controller.ts
+++ scratch/src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
 
 // Handles incoming requests
-@Controller()
+// /app/asdf, /app/byeにアクセスできる
+@Controller('/app')
 export class AppController {
-  @Get()
+  @Get('/asdf')
   getRootRoute() {
     return "hi there";
+  }
+
+  @Get('/bye')
+  getByeThere() {
+    return 'bye there!'
   }
 }
