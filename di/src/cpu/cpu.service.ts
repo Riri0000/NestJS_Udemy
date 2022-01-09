76187ff32af2ca4d993b858ceef741ab03d861af
Riri0000 <ririko010102@gmail.com>
2022-01-09 09:25:07 +0900

    Add: Set up dependency injection between different modules

--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  // 3. Define the constructor method on CpuService and add PoowerService to it
+  constructor(private powerService: PowerService) {}
+}
+
--- di/src/power/power.module.ts
+++ di/src/power/power.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  // 1. Add PowerService to the PowerModule's list of exports
+  exports: [PowerService]
 })
 export class PowerModule {}
