737fe40a959a0bf1f7dbad630f7fff604b2cdb2b
Riri0000 <ririko010102@gmail.com>
2022-01-09 09:39:30 +0900

    Add: DI between Modules

--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -4,6 +4,12 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   // 3. Define the constructor method on CpuService and add PoowerService to it
-  constructor(private powerService: PowerService) {}
+  constructor(private powerService: PowerService) { }
+
+  compute(a: number, b: number) {
+    console.log(`Drawing 10 watts of power from Power service`);
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
 
--- di/src/power/power.module.ts
+++ di/src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
+  // providers only accessable inside the module
   providers: [PowerService],
   // 1. Add PowerService to the PowerModule's list of exports
   exports: [PowerService]
