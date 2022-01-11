067750fa41e155fea8df9aca5dbc82ac173e7af3
Riri0000 <ririko010102@gmail.com>
2022-01-11 20:37:16 +0900

    Add: comments

--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -8,9 +8,11 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {
     this.repo = repo;
   }
+  // create entity instance
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
+    // save to DB
     return this.repo.save(user);
   }
 }
