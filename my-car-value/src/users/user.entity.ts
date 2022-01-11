f08094f58cba50c533843cba63e553b2b64c3110
Riri0000 <ririko010102@gmail.com>
2022-01-11 20:46:40 +0900

    Add: Hooksのテスト

--- my-car-value/src/users/user.entity.ts
+++ my-car-value/src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, AfterRemove, Entity, Column, PrimaryGeneratedColumn, AfterUpdate } from 'typeorm';
 
 // Create an entity file, and create a class in it that lists all the properties that your entity will have.
 @Entity()
@@ -11,4 +11,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id)
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id)
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id)
+  }
 }
