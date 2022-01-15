fd26a8850fe3b074bef7d263c402f3b63d22ee8a
Riri0000 <ririko010102@gmail.com>
2022-01-15 14:09:57 +0900

    Add: cookie

--- my-car-value/src/main.ts
+++ my-car-value/src/main.ts
@@ -1,15 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['aasdfia'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       // 許可されていないプロパティをPOSTしない
-      whitelist: true
-    })
-  )
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
